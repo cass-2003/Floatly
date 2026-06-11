7fc70305f57fbd9dd0c22cf19686b853edc223b5
Administrator <admin@desklite.local>
2026-06-12 00:18:26 +0800

    chore(release): bump version to 2.0.3

--- build-release.ps1
+++ build-release.ps1
@@ -1,9 +1,9 @@
-# Floatly v2.0.0 Build Script
+# Floatly v2.0.3 Build Script
 # PowerShell 5.1+
 
 $ErrorActionPreference = "Stop"
 
-$Version = "2.0.2"
+$Version = "2.0.3"
 $RootDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $ProjectDir = Join-Path $RootDir "DeskLite"
 $OutputDir = Join-Path $RootDir "release\Floatly"
--- installer/Floatly.iss
+++ installer/Floatly.iss
@@ -1,6 +1,6 @@
 ﻿#define MyAppName "Floatly"
 #define MyAppDisplayName "Floatly（浮岛）"
-#define MyAppVersion "2.0.2"
+#define MyAppVersion "2.0.3"
 #define MyAppPublisher "cass-2003"
 #define MyAppURL "https://github.com/cass-2003/Floatly"
 #define MyAppExeName "Floatly.exe"
